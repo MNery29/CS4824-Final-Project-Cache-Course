21fb1ea57c06f5462de4ae7c6148c66e1f8dbf81
whw2112 <whw2112@micro02.ee.columbia.edu>
2025-03-29 15:51:26 -0400

    made dcache file

---
+++ csee4824-project-4/verilog/dcache.sv
@@ -0,0 +1,9 @@
+/*
+dcache specifications:
+    256-bytes, 32 lines
+    non-blocking
+    write-back
+    set-associative
+    z-cache design -- https://people.csail.mit.edu/sanchez/papers/2010.zcache.micro.pdf
+
+*/
