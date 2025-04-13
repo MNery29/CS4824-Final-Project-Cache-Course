1c1efc5e5825c7cccccc0b006dab6b8ac5d32b22
whw2112 <whw2112@micro02.ee.columbia.edu>
2025-04-13 16:47:46 -0400

    fixed some errors on dcache implemnetation, working on making it non-blocking and writeback

--- csee4824-project-4/verilog/dcache.sv
+++ csee4824-project-4/verilog/dcache.sv
@@ -4,7 +4,7 @@ dcache specifications:
     directly mapped
     non-blocking
     write-through
-    cache line size of 16 bytes, 32 lines
+    cache line size of 8 bytes, 32 lines
     16 bytes / 8 bytes = 2 = 1 offset length
     32 lines = 5 index bits
     for a 32 bit address length: { Tag(26 bits) | Index(5 bits) | Offset(1 bits) }
@@ -15,14 +15,14 @@ dcache specifications:
 module dcache
 #(
     TAG_BITS = 26,
-    INDEX_BITS = 5,
-    OFFSET_BITS = 1
+    INDEX_BITS = 6,
+    OFFSET_BITS = 0
 )
 (
     input clk;
     input reset;
 
-     input [`XLEN-1:0] proc2Dcache_addr,
+    input [`XLEN-1:0] proc2Dcache_addr,
 
     input [3:0]  mem2proc_response, // 0 = can't accept, other=tag of transaction
     input [63:0] mem2proc_data,     // data resulting from a load
@@ -31,8 +31,11 @@ module dcache
     output [`XLEN-1:0] proc2mem_addr,
     output [63:0]      proc2mem_data, // address for current command
     output [1:0]       proc2mem_command, // `BUS_NONE `BUS_LOAD or `BUS_STORE
-    output MEM_SIZE    proc2mem_size
+    output MEM_SIZE    proc2mem_size,
 
+    output logic [63:0] hit_data, // data resulting from a load
+    output logic hit, // 1 if hit, 0 if miss
+    output logic [3:0] data_tag,
 );
     logic [TAG_BITS-1:0] addr_tag;
     logic [INDEX_BITS-1:0] addr_index;
@@ -42,15 +45,12 @@ module dcache
     always_comb begin
         addr_tag    = proc2Dcache_addr[`XLEN-1 : INDEX_BITS + OFFSET_BITS];
         addr_index  = proc2Dcache_addr[INDEX_BITS + OFFSET_BITS - 1 : OFFSET_BITS];
-        addr_offset = proc2Dcache_addr[OFFSET_BITS-1 : 0];
+        //addr_offset = proc2Dcache_addr[OFFSET_BITS-1 : 0];
     end
 
-    logic [127:0] cache_data [0:31]; // 32 lines of 16 bytes
+    logic [63:0] cache_data [0:63]; // 32 lines of 16 bytes
     logic [TAG_BITS-1:0] cache_tag [0:63]; // 32 lines of tag bits
     logic cache_valid [0:63]; // 32 lines of valid bits
-
-    logic hit;
-    logic [127:0] hit_data;
 
     logic [1:0] state;
     logic [1:0] next_state;
@@ -84,7 +84,7 @@ module dcache
             //is there a better way to do this?
             integer i;
             for (i=0; i<32; i++) begin
-                cache_data[i]  <= 128'b0;
+                cache_data[i]  <= 64'b0;
                 cache_tag[i]   <= {TAG_BITS{1'b0}};
                 cache_valid[i] <= 1'b0;
             end
@@ -93,11 +93,11 @@ module dcache
         end
         else begin
             state <= next_state;
-            if (state == `IDLE && !hit) begin 
+            if (next_state == `MISS && !hit) begin 
                 saved_addr <= proc2Dcache_addr;
             end
-            if (state == `WAIT && mem2proc_tag != 0) begin
-                cache_data[saved_addr[INDEX_BITS+OFFSET_BITS-1 : OFFSET_BITS]]  <= { mem2proc_data, mem2proc_data };
+            if (next_state == `IDLE && mem2proc_tag != 0) begin
+                cache_data[saved_addr[INDEX_BITS+OFFSET_BITS-1 : OFFSET_BITS]]  <= mem2proc_data;
                 cache_tag[saved_addr[INDEX_BITS+OFFSET_BITS-1 : OFFSET_BITS]]   <= saved_addr[31:INDEX_BITS+OFFSET_BITS];
                 cache_valid[saved_addr[INDEX_BITS+OFFSET_BITS-1 : OFFSET_BITS]] <= 1'b1;
             end
