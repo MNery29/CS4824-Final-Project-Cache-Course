9ae1ba195fa4b3d6ca9303c2eb4f37425fbd20f7
M.Nery29 <151114051+MNery29@users.noreply.github.com>
2025-04-17 00:10:26 -0400

    initial pipeline modifications

--- csee4824-project-4/verilog/pipeline.sv
+++ csee4824-project-4/verilog/pipeline.sv
@@ -3,8 +3,8 @@
 //   Modulename :  pipeline.sv                                         //
 //                                                                     //
 //  Description :  Top-level module of the verisimple pipeline;        //
-//                 This instantiates and connects the 5 stages of the  //
-//                 Verisimple pipeline together.                       //
+//                 This instantiates and connects the 6 stages of our  //
+//                 P6 pipeline together, along with the needed modules //
 //                                                                     //
 /////////////////////////////////////////////////////////////////////////
 
@@ -20,167 +20,175 @@ module pipeline (
     output logic [1:0]       proc2mem_command, // Command sent to memory
     output logic [`XLEN-1:0] proc2mem_addr,    // Address sent to memory
     output logic [63:0]      proc2mem_data,    // Data sent to memory
-`ifndef CACHE_MODE // no longer sending size to memory
+`ifndef CACHE_MODE
     output MEM_SIZE          proc2mem_size,    // Data size sent to memory
 `endif
 
-    // Note: these are assigned at the very bottom of the module
     output logic [3:0]       pipeline_completed_insts,
     output EXCEPTION_CODE    pipeline_error_status,
     output logic [4:0]       pipeline_commit_wr_idx,
     output logic [`XLEN-1:0] pipeline_commit_wr_data,
     output logic             pipeline_commit_wr_en,
     output logic [`XLEN-1:0] pipeline_commit_NPC
-
-    // Debug outputs: these signals are solely used for debugging in testbenches
-    // Do not change for project 3
-    // You should definitely change these for project 4
-    // output logic [`XLEN-1:0] if_NPC_dbg,
-    // output logic [31:0]      if_inst_dbg,
-    // output logic             if_valid_dbg,
-    // output logic [`XLEN-1:0] if_id_NPC_dbg,
-    // output logic [31:0]      if_id_inst_dbg,
-    // output logic             if_id_valid_dbg,
-    // output logic [`XLEN-1:0] id_ex_NPC_dbg,
-    // output logic [31:0]      id_ex_inst_dbg,
-    // output logic             id_ex_valid_dbg,
-    // output logic [`XLEN-1:0] ex_mem_NPC_dbg,
-    // output logic [31:0]      ex_mem_inst_dbg,
-    // output logic             ex_mem_valid_dbg,
-    // output logic [`XLEN-1:0] mem_wb_NPC_dbg,
-    // output logic [31:0]      mem_wb_inst_dbg,
-    // output logic             mem_wb_valid_dbg
 );
 
     //////////////////////////////////////////////////
-    //                                              //
-    //                Pipeline Wires                //
-    //                                              //
+    //                IF Stage Wires                //
     //////////////////////////////////////////////////
-
-    // Pipeline register enables
-    logic if_id_enable, id_ex_enable, ex_mem_enable, mem_wb_enable;
-
-    // Outputs from IF-Stage and IF/ID Pipeline Register
     logic [`XLEN-1:0] proc2Icache_addr;
     logic [63:0]      Icache_data_out;
     logic             Icache_valid_out;
+    logic             if_valid;
+    logic             take_branch;
+    logic [`XLEN-1:0] branch_target;
+    logic             stall_if;
+    IF_ID_PACKET      if_packet;
+
+    //////////////////////////////////////////////////
+    //                ID Stage Wires                //
+    //////////////////////////////////////////////////
+    ID_IS_PACKET      id_is_packet;
+    IF_ID_PACKET      if_id_reg;
+
+    logic [45:0]      id_rob_debug[31:0];
+    logic [11:0]      id_rob_pointers;
+    logic [7:0]       id_mt_tags[31:0];
+    logic [74:0]      id_rs_debug;
+
+    //////////////////////////////////////////////////
+    //                IS Stage Wires                //
+    //////////////////////////////////////////////////
+    logic [`RS_SIZE-1:0] rs_ready_out;
+    logic [31:0]         rs_opa_out   [`RS_SIZE];
+    logic [31:0]         rs_opb_out   [`RS_SIZE];
+    logic [5:0]          rs_tag_out   [`RS_SIZE];
+    ALU_FUNC             rs_alu_func_out[`RS_SIZE];
+    logic [31:0]         rs_npc_out     [`RS_SIZE];
+    logic [31:0]         rs_inst_out    [`RS_SIZE];
+
+    logic fu_ready = 1'b1;
+    logic [`RS_SIZE-1:0] rs_issue_enable;
+    IS_EX_PACKET is_packet;
+    logic issue_valid;
+
+
+    //////////////////////////////////////////////////
+    //               I-Cache Wires                  //
+    //////////////////////////////////////////////////
     logic [1:0]       proc2Imem_command;
     logic [`XLEN-1:0] proc2Imem_addr;
-    logic [3:0]  Imem2proc_response; // Should be zero unless there is a response
-    logic [63:0] Imem2proc_data;
-    logic [3:0]  Imem2proc_tag;
+    logic [3:0]       Imem2proc_response;
+    logic [63:0]      Imem2proc_data;
+    logic [3:0]       Imem2proc_tag;
 
-    always_comb begin
-        if (proc2Dmem_command != BUS_NONE) begin // read or write DATA from memory
-            proc2mem_command = proc2Dmem_command;
-            proc2mem_addr    = proc2Dmem_addr;
-            proc2mem_size    = proc2Dmem_size;  // size is never DOUBLE in project 3
-        end else begin                          // read an INSTRUCTION from memory
-            proc2mem_command = BUS_LOAD;
-            proc2mem_addr    = proc2Imem_addr;
-            proc2mem_size    = DOUBLE;          // instructions load a full memory line (64 bits)
-        end
-        proc2mem_data = {32'b0, proc2Dmem_data};
-    end
+    //////////////////////////////////////////////////
+    //           Temporary Branch Logic             //
+    //////////////////////////////////////////////////
+    assign if_valid = 1'b1;                // Always fetch for now
+    assign take_branch = 1'b0;             // No branch resolution yet
+    assign branch_target = 32'b0;          // Default branch target
 
-    IF_ID_PACKET if_packet, if_id_reg;
-
+    //////////////////////////////////////////////////
+    //         Fetch Stage                          //
+    //////////////////////////////////////////////////
     stage_if stage_if_0 (
-        // Inputs
-        .clock (clock),
-        .reset (reset),
-        .if_valid       (next_if_valid),
-        .take_branch    (ex_mem_reg.take_branch),
-        .branch_target  (ex_mem_reg.alu_result),
-        .Icache_data_out (Icache_data_out),
-        .Icache_valid_out (Icache_valid_out),
-
-        // Outputs
-        .if_packet      (if_packet),
-        .proc2Icache_addr (proc2Icache_addr)
+        .clock(clock),
+        .reset(reset),
+        .if_valid(if_valid),
+        .take_branch(take_branch),
+        .branch_target(branch_target),
+        .Icache_data_out(Icache_data_out),
+        .Icache_valid_out(Icache_valid_out),
+        .if_packet(if_packet),
+        .proc2Icache_addr(proc2Icache_addr),
+        .stall_if(stall_if)
     );
 
+    //////////////////////////////////////////////////
+    //                  I-Cache                     //
+    //////////////////////////////////////////////////
     icache icache_0 (
-        //input
-        .clock           (clock),
-        .reset           (reset),
-        //input
-        .Imem2proc_response(Imem2proc_response),
-        .Imem2proc_data    (Imem2proc_data),
-        .Imem2proc_tag     (Imem2proc_tag),
-        //input
-        .proc2Icache_addr  (proc2Icache_addr),  
-        //output
-        .proc2Imem_command (proc2Imem_command),
-        .proc2Imem_addr    (proc2Imem_addr),
-        //output
-        .Icache_data_out   (Icache_data_out),
-        .Icache_valid_out  (Icache_valid_out)
+        .clock(clock),
+        .reset(reset),
+        .Imem2proc_response(mem2proc_response),
+        .Imem2proc_data(mem2proc_data),
+        .Imem2proc_tag(mem2proc_tag),
+        .proc2Icache_addr(proc2Icache_addr),
+        .proc2Imem_command(proc2Imem_command),
+        .proc2Imem_addr(proc2Imem_addr),
+        .Icache_data_out(Icache_data_out),
+        .Icache_valid_out(Icache_valid_out)
     );
 
-    // Outputs from ID stage and ID/EX Pipeline Register
-    ID_EX_PACKET id_packet, id_ex_reg;
+    //////////////////////////////////////////////////
+    //               Decode Stage                   //
+    //////////////////////////////////////////////////
+    stage_id stage_id_0 (
+        .clock(clock),
+        .reset(reset),
+        .if_id_reg(if_packet),
+        .id_is_packet(id_is_packet),
+        .rob_debug(id_rob_debug),
+        .rob_pointers_debug(id_rob_pointers),
+        .mt_tags_debug(id_mt_tags),
+        .rs_debug(id_rs_debug)
+    );
 
-    // Outputs from EX-Stage and EX/MEM Pipeline Register
-    EX_MEM_PACKET ex_packet, ex_mem_reg;
+    //////////////////////////////////////////////////
+    //                Issue Stage                   //
+    //////////////////////////////////////////////////
+    stage_is stage_is_0 (
+        .clock(clock),
+        .reset(reset),
+        .rs_ready_out(id_is_packet.rs_ready),
+        .rs_opa_out(id_is_packet.opa),
+        .rs_opb_out(id_is_packet.opb),
+        .rs_tag_out(id_is_packet.dest_tag),
+        .rs_alu_func_out(id_is_packet.alu_func),
+        .rs_npc_out(id_is_packet.NPC),
+        .rs_inst_out(id_is_packet.inst),
+        .fu_ready(fu_ready),
+        .issue_valid(issue_valid),
+        .is_packet(is_packet),
+        .rs_issue_enable(rs_issue_enable)
+    );
 
-    // Outputs from MEM-Stage and MEM/WB Pipeline Register
-    MEM_WB_PACKET mem_packet, mem_wb_reg;
 
-    // Outputs from MEM-Stage to memory
+    //////////////////////////////////////////////////
+    //              Memory Access Logic             //
+    //////////////////////////////////////////////////
     logic [`XLEN-1:0] proc2Dmem_addr;
     logic [`XLEN-1:0] proc2Dmem_data;
     logic [1:0]       proc2Dmem_command;
+`ifndef CACHE_MODE
     MEM_SIZE          proc2Dmem_size;
-
-    // Outputs from WB-Stage (These loop back to the register file in ID)
-    logic             wb_regfile_en;
-    logic [4:0]       wb_regfile_idx;
-    logic [`XLEN-1:0] wb_regfile_data;
-
-    //////////////////////////////////////////////////
-    //                                              //
-    //                Memory Outputs                //
-    //                                              //
-    //////////////////////////////////////////////////
-
-    // these signals go to and from the processor and memory
-    // we give precedence to the mem stage over instruction fetch
-    // note that there is no latency in project 3
-    // but there will be a 100ns latency in project 4
+`endif
 
     always_comb begin
-        if (proc2Dmem_command != BUS_NONE) begin // read or write DATA from memory
+        if (proc2Dmem_command != BUS_NONE) begin
             proc2mem_command = proc2Dmem_command;
             proc2mem_addr    = proc2Dmem_addr;
 `ifndef CACHE_MODE
-            proc2mem_size    = proc2Dmem_size;  // size is never DOUBLE in project 3
+            proc2mem_size    = proc2Dmem_size;
 `endif
-        end else begin                          // read an INSTRUCTION from memory
+        end else begin
             proc2mem_command = BUS_LOAD;
             proc2mem_addr    = proc2Imem_addr;
 `ifndef CACHE_MODE
-            proc2mem_size    = DOUBLE;          // instructions load a full memory line (64 bits)
+            proc2mem_size    = DOUBLE;
 `endif
         end
         proc2mem_data = {32'b0, proc2Dmem_data};
     end
 
     //////////////////////////////////////////////////
-    //                                              //
     //               Pipeline Outputs               //
-    //                                              //
     //////////////////////////////////////////////////
-
-    assign pipeline_completed_insts = {3'b0, mem_wb_reg.valid}; // commit one valid instruction
-    assign pipeline_error_status = mem_wb_reg.illegal        ? ILLEGAL_INST :
-                                   mem_wb_reg.halt           ? HALTED_ON_WFI :
-                                   (mem2proc_response==4'h0) ? LOAD_ACCESS_FAULT : NO_ERROR;
-
-    assign pipeline_commit_wr_en   = wb_regfile_en;
-    assign pipeline_commit_wr_idx  = wb_regfile_idx;
-    assign pipeline_commit_wr_data = wb_regfile_data;
-    assign pipeline_commit_NPC     = mem_wb_reg.NPC;
+    assign pipeline_completed_insts = 4'd0; // placeholder
+    assign pipeline_error_status    = NO_ERROR;
+    assign pipeline_commit_wr_en    = 1'b0;
+    assign pipeline_commit_wr_idx   = 5'd0;
+    assign pipeline_commit_wr_data  = 64'd0;
+    assign pipeline_commit_NPC      = 64'd0;
 
 endmodule // pipeline
